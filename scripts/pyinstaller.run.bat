896fd4341a74259d5af79ccd046e13360f84f893
piotrj <piotrj@alatek.krakow.pl>
2023-12-22 12:02:04 +0100

    exe version & other details processing by pyinstaller

--- scripts/pyinstaller.run.bat
+++ scripts/pyinstaller.run.bat
@@ -21,11 +21,11 @@
 
 @echo.
 @echo running-pyinstaller-stage_librer
-pyinstaller --noconfirm --clean --add-data="distro.info.txt:." --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR% --windowed --contents-directory=internal librer.py  || exit /b 2
+pyinstaller --version-file=version.pi.librer.txt --noconfirm --clean --add-data="distro.info.txt:." --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR% --windowed --contents-directory=internal librer.py  || exit /b 2
 
 @echo.
 @echo running-pyinstaller-stage_record
-pyinstaller --noconfirm --clean --add-data="distro.info.txt:." --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR% --console --hide-console hide-early --contents-directory=internal record.py  || exit /b 1
+pyinstaller --version-file=version.pi.record.txt --noconfirm --clean --add-data="distro.info.txt:." --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR% --console --hide-console hide-early --contents-directory=internal record.py  || exit /b 1
 
 move %OUTDIR%\record\record.exe %OUTDIR%\librer
 
