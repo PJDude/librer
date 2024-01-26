4c0364ec1217263c59cfd3772849364c53ccebf8
piotrj <pj.soft.dev.x@gmail.com>
2024-01-26 18:27:27 +0100

    fix path

--- scripts/nuitka.run.bat
+++ scripts/nuitka.run.bat
@@ -21,11 +21,11 @@ python -m nuitka --version >> distro.info.txt
 
 @echo.
 @echo running-nuitka-stage_librer
-python -m nuitka --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --enable-plugin=tk-inter --lto=yes --follow-stdlib  --assume-yes-for-downloads --windows-disable-console --output-dir=$outdir --standalone --output-filename=librer ./librer.py
+python -m nuitka --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --enable-plugin=tk-inter --lto=yes --follow-stdlib  --assume-yes-for-downloads --windows-disable-console --output-dir=%outdir% --standalone --output-filename=librer ./librer.py
 
 @echo.
 @echo running-nuitka-stage_record
-python -m nuitka --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --output-dir=$outdir --lto=yes --follow-stdlib  --assume-yes-for-downloads --standalone ./record.py --output-filename=record
+python -m nuitka --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --output-dir=%outdir% --lto=yes --follow-stdlib  --assume-yes-for-downloads --standalone ./record.py --output-filename=record
 
 move %OUTDIR%\record.dist\record.exe %OUTDIR%\librer.dist
 move %OUTDIR%\librer.dist %OUTDIR%\librer
